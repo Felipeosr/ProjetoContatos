e392e08a6f1311de4ac8fb40b764c1cd3502fcff
Felipeosr <felipe@show.onmicrosoft.com>
2023-03-23 20:34:29 -0300

    Primeiro commit

---
+++ CrontroleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrontroleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
---
+++ CrontroleDeContatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CrontroleDeContatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
